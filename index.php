c9aede00e4acb11aeb6f4bfd21e172738681b446
Lucas <lucaspatrocinio@hotmail.com>
2019-11-24 14:38:23 -0300

    Subindo os arquivos inicias

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Trab</title>
+</head>
+<body>
+    <div class="corpo" style="background-color: #ccc; text-align: center;">
+        <h1>Página principal do site</h1>
+    </div>
+
+</body>
+</html>
